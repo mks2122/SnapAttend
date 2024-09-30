1f6e37cc4009c72be384205804efab0f602c012b
Karthick Sriram <karthicksriram71@gmail.com>
2024-09-30 10:36:51 +0530

    initial commit

---
+++ AttendRx-Face-Recognition-Attendance-System/ESP32Cam_Code_AttendRx/AttendRx/WifiCam.hpp
@@ -0,0 +1,17 @@
+#include <dummy.h>
+
+#ifndef WIFICAM_HPP
+#define WIFICAM_HPP
+
+// #include <esp32cam.h>
+
+#include <WebServer.h>
+
+extern esp32cam::Resolution initialResolution;
+
+extern WebServer server;
+
+void
+addRequestHandlers();
+
+#endif // WIFICAM_HPP
